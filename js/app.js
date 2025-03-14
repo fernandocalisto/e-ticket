cf74680fcb2c09960eef1889ae8bc976fbe603c4
Fernando Calisto <fernandocalisto27@gmail.com>
2025-03-14 16:33:53 -0300

    feat(compra): Verificar quantidade

--- js/app.js
+++ js/app.js
@@ -2,28 +2,38 @@ let escolha = document.getElementById('tipo-ingresso');
 let quantidade = document.getElementById('qtd');
 
 function comprar(){
-    if (escolha.value == 'inferior'){
-        if (quantidade.value > parseInt(document.getElementById('qtd-inferior').textContent)){
-            alert('Ingressos Insuficientes!');
-        }
-        else{
-            document.getElementById('qtd-inferior').textContent = parseInt(document.getElementById('qtd-inferior').textContent) - parseInt(quantidade.value);
-        }
+
+    if (quantidade.value > 10){
+        alert('Limite de compra de 10 ingressos por vez!');
+        quantidade.value = '';
     }
-    else if (escolha.value == 'superior'){
-        if (quantidade.value > parseInt(document.getElementById('qtd-superior').textContent)){
-            alert('Ingressos Insuficientes!');
-        }
-        else{
-            document.getElementById('qtd-superior').textContent = parseInt(document.getElementById('qtd-superior').textContent) - parseInt(quantidade.value);
-        }
+    else if (quantidade.value == ''){
+        alert('Insira a quantidade desejada!');
     }
-    else {
-        if (quantidade.value > parseInt(document.getElementById('qtd-pista').textContent)){
-            alert('Ingressos Insuficientes!');
+    else{
+        if (escolha.value == 'inferior'){
+            if (quantidade.value > parseInt(document.getElementById('qtd-inferior').textContent)){
+                alert('Ingressos Insuficientes!');
+            }
+            else{
+                document.getElementById('qtd-inferior').textContent = parseInt(document.getElementById('qtd-inferior').textContent) - parseInt(quantidade.value);
+            }
         }
-        else{
-            document.getElementById('qtd-pista').textContent = parseInt(document.getElementById('qtd-pista').textContent) - parseInt(quantidade.value);
+        else if (escolha.value == 'superior'){
+            if (quantidade.value > parseInt(document.getElementById('qtd-superior').textContent)){
+                alert('Ingressos Insuficientes!');
+            }
+            else{
+                document.getElementById('qtd-superior').textContent = parseInt(document.getElementById('qtd-superior').textContent) - parseInt(quantidade.value);
+            }
+        }
+        else {
+            if (quantidade.value > parseInt(document.getElementById('qtd-pista').textContent)){
+                alert('Ingressos Insuficientes!');
+            }
+            else{
+                document.getElementById('qtd-pista').textContent = parseInt(document.getElementById('qtd-pista').textContent) - parseInt(quantidade.value);
+            }
         }
     }
 }
