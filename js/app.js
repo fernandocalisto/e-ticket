903243f62b3f577256aaef0aa79d191ee1eaef83
Fernando Calisto <fernandocalisto27@gmail.com>
2025-03-14 16:29:28 -0300

    feat(compra): Comprar Ingressos

---
+++ js/app.js
@@ -0,0 +1,29 @@
+let escolha = document.getElementById('tipo-ingresso');
+let quantidade = document.getElementById('qtd');
+
+function comprar(){
+    if (escolha.value == 'inferior'){
+        if (quantidade.value > parseInt(document.getElementById('qtd-inferior').textContent)){
+            alert('Ingressos Insuficientes!');
+        }
+        else{
+            document.getElementById('qtd-inferior').textContent = parseInt(document.getElementById('qtd-inferior').textContent) - parseInt(quantidade.value);
+        }
+    }
+    else if (escolha.value == 'superior'){
+        if (quantidade.value > parseInt(document.getElementById('qtd-superior').textContent)){
+            alert('Ingressos Insuficientes!');
+        }
+        else{
+            document.getElementById('qtd-superior').textContent = parseInt(document.getElementById('qtd-superior').textContent) - parseInt(quantidade.value);
+        }
+    }
+    else {
+        if (quantidade.value > parseInt(document.getElementById('qtd-pista').textContent)){
+            alert('Ingressos Insuficientes!');
+        }
+        else{
+            document.getElementById('qtd-pista').textContent = parseInt(document.getElementById('qtd-pista').textContent) - parseInt(quantidade.value);
+        }
+    }
+}
